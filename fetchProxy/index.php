03029fef2da7c4f8dbb03545264db2e144cc4683
abhaygupta08 <abhay.19201@knit.ac.in>
2021-02-26 11:52:46 +0530

    added apiDocs,some fetchAPI fixes

--- fetchProxy/index.php
+++ fetchProxy/index.php
@@ -5,7 +5,8 @@ $key = "";
 	
 try{
 	$key = $_GET['key'];
-	if($key==="")
+	$proxyType = $_GET['proxyType'];
+	if($key==="" and $proxyType==="" )
 		{
 			exit("Message: NO KEY ACCESS");
 			throw new Exception("INVALID ACCESS");
@@ -21,16 +22,28 @@ catch(Exception $e) {
 
 if($key==="test123" or $key==="test111" or $key==="test222"){
 	
-	fetchContent($key,$token);
+	fetchContent($key,$proxyType);
 }
 else{
 	echo "Message: INVALID KEY";
 
 }
 
-function fetchContent($key="",$token=""){
-	$file = fopen("../proxy.txt","r") or die("Message: UNABLE TO OPEN FILE(FILE REMOVED FROM SERVER)");
-	for($i=0;$i<filesize("../proxy.txt");$i++){
+function fetchContent($key="",$proxyType="http"){
+	if($proxyType==="http"){
+		$filename = '../proxyHTTP.txt';
+	}
+	elseif ($proxyType==="socks4") {
+	$filename = '../proxySOCKS4.txt';
+	}
+	elseif ($proxyType==="socks5") {
+	$filename = '../proxySOCKS5.txt';
+	}
+	else{
+		exit("Message: ERROR in proxyType");
+	}
+	$file = fopen($filename,"r") or die("Message: UNABLE TO OPEN FILE(FILE REMOVED FROM SERVER)");
+	for($i=0;$i<filesize($filename);$i++){
 		echo fgetc($file);
 	}
 }
