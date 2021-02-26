aa1b649dbad6299b1c295da566d3b654918d9d85
abhaygupta08 <abhay.19201@knit.ac.in>
2021-02-26 11:58:52 +0530

    apiDocs,fixed apiFetch

--- dashboard.php
+++ dashboard.php
@@ -31,9 +31,24 @@ Message: INVALID ACCESS";
 function fetchContent($key="",$token=""){
 	echo "
 	<center>
-	<h1>WELCOME newbie</h1><hr>
-	<br><br>Your URL is Below :<br><br>";
-	print_r($_SERVER['SERVER_NAME']."/fetchProxy/?key=".$key."</center>");
+	<h1>WELCOME TECHIE (PUBLIC KEY ACCESS)</h1><hr>
+	<br><br>Your API URL is Below :<br><br>";
+	
+	echo "<br><br>HTTP/S API :<br>";
+	print_r($_SERVER['SERVER_NAME']."/fetchProxy/?key=".$key."&proxyType=http");
+	$url = $_SERVER['SERVER_NAME']."/fetchProxy/?key=".$key."&proxyType=http";
+	print_r("<br><form action=\"".$url."\"><button type=\"submit\">OPEN HTTP PROXIES</button></form>");
+
+	echo "<br><br>SOCKS4 API :<br>";
+	print_r($_SERVER['SERVER_NAME']."/fetchProxy/?key=".$key."&proxyType=socks4");
+	$url = $_SERVER['SERVER_NAME']."/fetchProxy/?key=".$key."&proxyType=socks4";
+	print_r("<br><form action=\"".$url."\"><button type=\"submit\">OPEN SOCKS4 PROXIES</button></form>");
+	
+	echo "<br><br>SOCKS5 API :<br>";
+	print_r($_SERVER['SERVER_NAME']."/fetchProxy/?key=".$key."&proxyType=socks5");
+	$url = $_SERVER['SERVER_NAME']."/fetchProxy/?key=".$key."&proxyType=socks5";
+	print_r("<br><form action=\"".$url."\"><button type=\"submit\">OPEN SOCKS5 PROXIES</button></form></center>");
+
 	if($_SERVER['SERVER_NAME']==="localhost"){
 	print_r("<center>FOR LOCALHOST : "."http://localhost/php_learn/20feb/fetchProxy/?key=".$key."</center>");
 	}
--- index.php
+++ index.php
@@ -4,17 +4,77 @@
 	<title>21 FEB LEARNING</title>
 </head>
 <body align="center">
-	HEADER 	<br><br><h2>
-	WELCOME CRACKER Ultra Max</h2><hr>
+	HEADER 	<br><br><hr>
 	<h1>PROXY LEECHER BY ABHAY</h1>
-	<hr><br><br><br>
+	<hr>
+	
+		<!-- <form action="" method="post"> -->
+			<button style="background: black;color:white;" name="APIDOCS" onclick="myFunction()">API</button>
+			<button style="background: black;color:white;" name="HOME" onclick="myFunction2()">LOGIN</button>
+		<!-- </form> -->
+
+<div id="myDIV">
+<h2>WELCOME CRACKER Ultra Max</h2><hr>
+<br><br><br>
 	<form action="dashboard.php" method="post">
 	<input type="text" name="key" placeholder="Enter Your Key Here" />
 	<input type="text" name="token" hidden value="testing123"/>
 	<input type="submit" value="AUTHENTICATE"/>
 	</form>
+	<br><br>
+</div>
+<div id="myAPI" style="display: none;">
+<h2>WELCOME CRACKER Ultra Max</h2><hr>
+<br>
+	PUBLIC ACCESS KEY : test123
+	<br><br>
+	<?php
+	$key = '<span style="color:red">{KEY}</span>';
+	echo "<br><br>DIRECT API URL :<br>";
+	print_r($key);
+	echo "<br><br>HTTP/S API :<br>";
+	print_r($_SERVER['SERVER_NAME']."/fetchProxy/?key=".$key."&proxyType=http");
+	echo "<br>";
 	
-	<br><br><br><hr>
+	echo "<br><br>SOCKS4 API :<br>";
+	print_r($_SERVER['SERVER_NAME']."/fetchProxy/?key=".$key."&proxyType=socks4");
+	echo "<br>";
+	
+	echo "<br><br>SOCKS5 API :<br>";
+	print_r($_SERVER['SERVER_NAME']."/fetchProxy/?key=".$key."&proxyType=socks5");
+	echo "<br><br>";
+	echo '<span style="color:blue;">Replace '.$key.'with your key.</span>';
+	?>
+</div>
+
+
+<script type="text/javascript">
+	function myFunction() {
+  var x = document.getElementById("myDIV");
+    x.style.display = "none";
+  var y = document.getElementById("myAPI");
+  	y.style.display = "block";
+}
+
+function myFunction2() {
+  var x = document.getElementById("myAPI");
+    x.style.display = "none";
+  var y = document.getElementById("myDIV");
+  	y.style.display = "block";
+}
+</script>
+<?php
+// if(isset($_POST['APIDOCS'])){
+// 	$homepage="";
+// 	$apiDocs = "HELO THERE !!!";	
+// }
+
+?>	
+	<br><hr>
 	FOOTER
+	<br><br>
+	<a href="https://t.me/technicaltitan" style="color:white;background: black;">CONTACT ME ON TG</a>
+	&nbsp;
+	<a href="https://github.com/abhaygupta08/proxyleecherPHP" style="color:white;background: black;" title="DON'T FORGET TO DROP STAR">SOURCE CODE</a>
 </body>
 </html>
